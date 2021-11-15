c25ab3afacfd6bda90ffa7f070e95f16144a1dcb
alex <alex@tekartik.com>
2021-11-15 20:33:06 +0100

    fix: ci

--- idb_sqflite_support/before_travis.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Must be ran from root (.travis.yml), will fail otherwise
-set -ev
-
-pub get
-pub run tekartik_travis_ci_flutter:install
--- idb_sqflite_support/tool/run_ci.dart
+++ idb_sqflite_support/tool/run_ci.dart
@@ -15,6 +15,7 @@ Future main() async {
   final nnbdEnabled = dartVersion > Version(2, 12, 0, pre: '0');
   if (nnbdEnabled) {
     for (var dir in [
+      'idb_sqflite_support',
       'idb_sqflite',
       'idb_sqflite_common_test',
       'idb_sqflite_test_app',
