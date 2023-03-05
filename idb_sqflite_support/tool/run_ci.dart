0ac6d1d45aac8fb6327eef689f5a21168cce19a1
Alexandre Roux <alex@tekartik.com>
2023-03-05 17:44:09 +0100

    dartsdk 2.18 strict

--- idb_sqflite_support/tool/run_ci.dart
+++ idb_sqflite_support/tool/run_ci.dart
@@ -1,9 +1,3 @@
-//
-
-//
-// This is to allow running this file without null experiment
-// In the future, remove this 2.9 comment or run using: dart --enable-experiment=non-nullable --no-sound-null-safety run tool/travis.dart
-
 import 'dart:io';
 
 import 'package:dev_test/package.dart';
